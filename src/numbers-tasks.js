17bac6d32adc63e02f7234823eb572f34fd17fb1
DaniilRai13 <testnet02@mail.ru>
2023-12-16 23:54:46 +0300

    fix:update file

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -137,8 +137,8 @@ function getLastDigit(value) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  return +value;
 }
 
 /**
@@ -175,8 +175,8 @@ function getParallelepipedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  return Math.round(num / 10 ** pow) * 10 ** pow;
 }
 
 /**
@@ -196,8 +196,15 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n <= 3) return true;
+  if (n > 3) {
+    for (let i = 2; i < n; i += 1) {
+      if (n % i === 0) return false;
+    }
+    return true;
+  }
+  return false;
 }
 
 /**
